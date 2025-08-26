3cdc58c0f540d0aa96ce9a6c44b004ada1cb70f1
Tran Manh Linh <devngmo@gmail.com>
2025-08-26 22:44:31 +0700

    SearchView: add controller

--- lib/widgets/search_view.dart
+++ lib/widgets/search_view.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
+class SearchViewController {
+  setText(String text) {
+    svState.setText(text);
+  }
+
+  late _SearchViewState svState;
+  void bind(_SearchViewState searchView) {
+    svState = searchView;
+  }
+}
+
 class SearchView extends StatefulWidget {
   final Function(String text) onTextChanged;
   final String hint;
-  const SearchView({super.key, required this.onTextChanged, this.hint = ""});
+  final SearchViewController controller;
+  const SearchView({super.key, required this.controller, required this.onTextChanged, this.hint = ""});
 
   @override
   State<SearchView> createState() => _SearchViewState();
@@ -11,8 +23,17 @@ class SearchView extends StatefulWidget {
 
 class _SearchViewState extends State<SearchView> {
   TextEditingController tec = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.bind(this);
+    debugPrint("SearchView.initState");
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("SearchView.build text=${tec.text}");
     return TextFormField(
       controller: tec,
       decoration: InputDecoration(
@@ -22,5 +43,9 @@ class _SearchViewState extends State<SearchView> {
         widget.onTextChanged(value);
       }
     );
+  }
+
+  void setText(String text) {
+    tec.text = text;
   }
 }
