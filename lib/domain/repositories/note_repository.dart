fdf8b0b9b9cfe8b2f35157c6a653bdd6cac93c16
Tran Manh Linh <devngmo@gmail.com>
2025-08-26 22:04:14 +0700

    use NoteFilterPageResult instead of List<NoteModel> for NoteRepository.filter return type

--- lib/domain/repositories/note_repository.dart
+++ lib/domain/repositories/note_repository.dart
@@ -2,11 +2,26 @@ import 'package:demonotes_getx/domain/models/note_model.dart';
 
 class NoteFilterQuery {
   final String? keyword;
-  NoteFilterQuery({this.keyword});
+  final int page;
+  final int pageSize;
+  NoteFilterQuery({this.keyword, this.pageSize = 20, this.page = 1});
+
+  bool isEquals(NoteFilterQuery query) {
+    if (keyword != query.keyword) return false;
+    if (page != query.page) return false;
+    if (pageSize != query.pageSize) return false;
+    return true;
+  }
+}
+
+class NoteFilterPageResult {
+  final NoteFilterQuery query;
+  final List<NoteModel> notes;
+  NoteFilterPageResult({required this.query, required this.notes});
 }
 
 abstract class NoteRepository {
-  Future<List<NoteModel>> filterAsync(NoteFilterQuery query) async {
-    return [];
+  Future<NoteFilterPageResult> filterAsync(NoteFilterQuery query) async {
+    return NoteFilterPageResult(query: query, notes: []);
   }
 }
