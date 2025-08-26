83b745c0ded89cf9384d72bcc7d3603ac29dce01
Tran Manh Linh <devngmo@gmail.com>
2025-08-26 22:45:16 +0700

    HomeController: add 3 list: - activeNotes - secretNotes - archivedNotes

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -2,6 +2,7 @@ import 'package:demonotes_getx/app/service_locator.dart';
 import 'package:demonotes_getx/common/models/StateCollectionData.dart';
 import 'package:demonotes_getx/domain/models/note_model.dart';
 import 'package:demonotes_getx/domain/repositories/note_repository.dart';
+import 'package:demonotes_getx/widgets/search_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
@@ -14,40 +15,97 @@ class HomePageBinding extends Bindings {
 }
 
 class HomeController extends GetxController {
-  var filteredNotes = FilterableStateCollectionData<NoteModel, NoteFilterQuery>(items: []).obs;
+  var activeNotes = FilterableStateCollectionData<NoteModel, NoteFilterQuery>(items: []).obs;
+  var archivedNotes = FilterableStateCollectionData<NoteModel, NoteFilterQuery>(items: []).obs;
+  var secretNotes = FilterableStateCollectionData<NoteModel, NoteFilterQuery>(items: []).obs;
 
+  List<NoteModel> get filteredNotes {
+    if (tabIndex == 0) return activeNotes.value.items;
+    else if (tabIndex == 1) return secretNotes.value.items;
+    return archivedNotes.value.items;
+  }
+
+  bool get isActiveTabLoading {
+    if (tabIndex == 0) return activeNotes.value.isProcessing;
+    else if (tabIndex == 1) return secretNotes.value.isProcessing;
+    return archivedNotes.value.isProcessing;
+  }
+  var _tabIndex = 0.obs;
+  int get tabIndex => _tabIndex.value;
+
+  SearchViewController svController = SearchViewController();
+
+  String get currentTabSearchKeyword {
+    if (tabIndex == 0) {
+      if (activeNotes.value.query == null) return "";
+      if (activeNotes.value.query!.keyword == null) return "";
+      return activeNotes.value.query!.keyword!;
+    }
+    if (tabIndex == 1) {
+      if (secretNotes.value.query == null) return "";
+      if (secretNotes.value.query!.keyword == null) return "";
+      return secretNotes.value.query!.keyword!;
+    }
+    if (archivedNotes.value.query == null) return "";
+    if (archivedNotes.value.query!.keyword == null) return "";
+    return archivedNotes.value.query!.keyword!;
+  }
   @override
   void onInit() {
     super.onInit();
     debugPrint("HomeController.onInit");
-    fetchNotes();
+    filterNotes();
   }
 
-  fetchNotes() {
+  filterNotes({String? keyword}) {
     debugPrint("HomeController.fetchNotes...");
-    NoteFilterQuery query = NoteFilterQuery();
-    filteredNotes.value = FilterableStateCollectionData(items: [], query: query, isProcessing: true);
+    if (tabIndex == 0)
+      fetchActiveNotes(keyword: keyword);
+    else if (tabIndex == 1)
+      fetchSecretNotes(keyword: keyword);
+    else
+      fetchArchivedNotes(keyword: keyword);
+  }
+
+  fetchActiveNotes({String? keyword}) {
+    NoteFilterQuery query = NoteFilterQuery(keyword: keyword);
+    activeNotes.value = FilterableStateCollectionData(items: [], query: query, isProcessing: true);
     serviceLocator.noteRepository.filterAsync(query)
         .then((result) {
-      debugPrint("  found ${result.notes.length} notes");
-      filteredNotes.value = FilterableStateCollectionData(items: result.notes, isProcessing: false);
+          if (activeNotes.value.query == null || activeNotes.value.query!.isEquals(result.query) == false) return;
+      debugPrint("  found ${result.notes.length} active notes");
+      activeNotes.value = FilterableStateCollectionData(items: result.notes, query: result.query, isProcessing: false);
     });
   }
 
-  void filter(String text) {
-    debugPrint("HomeController.filter keyword=$text...");
-    NoteFilterQuery query = NoteFilterQuery(keyword: text.trim());
-    filteredNotes.value = FilterableStateCollectionData(items: [], query: query, isProcessing: true);
+  fetchSecretNotes({String? keyword}) {
+    NoteFilterQuery query = NoteFilterQuery(keyword: keyword);
+    secretNotes.value = FilterableStateCollectionData(
+        items: [], query: query, isProcessing: true);
     serviceLocator.noteRepository.filterAsync(query)
         .then((result) {
-      debugPrint("  found ${result.notes.length} notes for query=${result.query.keyword}");
-
-          if (!filteredNotes.value.query!.isEquals(query)) {
-            debugPrint(" SKIP this");
-            return;
-          }
-
-      filteredNotes.value = FilterableStateCollectionData(items: result.notes, isProcessing: false);
+      if (secretNotes.value.query == null || secretNotes.value.query!.isEquals(result.query) == false) return;
+      debugPrint("  found ${result.notes.length} secret notes");
+      secretNotes.value = FilterableStateCollectionData(
+          items: result.notes, query: result.query, isProcessing: false);
     });
+  }
+
+  fetchArchivedNotes({String? keyword}) {
+    NoteFilterQuery query = NoteFilterQuery(keyword: keyword);
+    archivedNotes.value = FilterableStateCollectionData(
+        items: [], query: query, isProcessing: true);
+    serviceLocator.noteRepository.filterAsync(query)
+        .then((result) {
+      if (archivedNotes.value.query == null || archivedNotes.value.query!.isEquals(result.query) == false) return;
+      debugPrint("  found ${result.notes.length} archived notes");
+      archivedNotes.value = FilterableStateCollectionData(
+          items: result.notes, query: result.query, isProcessing: false);
+    });
+  }
+
+  void selectTabAt(int index) {
+    _tabIndex.value = index;
+    svController.setText(currentTabSearchKeyword);
   }
 }
