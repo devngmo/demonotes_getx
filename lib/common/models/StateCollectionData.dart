b02d6dd3542f5cf586f6bcff57e65db868f9185a
Tran Manh Linh <devngmo@gmail.com>
2025-08-26 22:03:27 +0700

    add FilterableStateCollectionData

--- lib/common/models/StateCollectionData.dart
+++ lib/common/models/StateCollectionData.dart
@@ -3,3 +3,8 @@ class StateCollectionData<T> {
   final List<T> items;
   StateCollectionData({required this.items, this.isProcessing = false});
 }
+
+class FilterableStateCollectionData<T, Q> extends StateCollectionData<T> {
+  final Q? query;
+  FilterableStateCollectionData({required super.items, this.query, super.isProcessing = false});
+}
